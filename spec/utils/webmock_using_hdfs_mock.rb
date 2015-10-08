d2b1b93cfc86f784ac93a5bf9d614c61b13f1ed8
Max Ziff <ziff@altiscale.com>
2015-10-08 12:09:15 -0700

    rubocoppery

--- spec/utils/webmock_using_hdfs_mock.rb
+++ spec/utils/webmock_using_hdfs_mock.rb
@@ -1,5 +1,5 @@
 #
-# Library: tbd
+# Library: webmock_using_hdfs_mock.rb
 #
 # Copyright (C) 2015 Altiscale, Inc.
 # Licensed under the Apache License, Version 2.0
@@ -9,75 +9,96 @@
 #
 
 #
-# TBD
+# webmock for dynamic hdfs mock
 #
-
-require 'addressable/template'
-
 module WebmockUsingHdfsMock
+  require 'addressable/template'
+
+  public
+
   def webmock_using_hdfs_mock(mockhdfs)
     @mockhdfs = mockhdfs
     header
     setup_environment
-    stub_requests
+    @general_t = Addressable::Template.new "http://#{@host}:#{@port}" +
+      '/webhdfs/v1{/segments*}?op={op}&user.name={user}'
+    stub_liststatus
+    stub_getfilestatus
+    stub_delete
+    stub_rename
   end
+
+  private
 
   def header
     @ctheader = { 'Content-Type' => 'application/json' }
 
-    @hostname = 'nn-cluster.nsdc.altiscale.com'
+    @host = 'nn-cluster.nsdc.altiscale.com'
     @port = '50070'
     @username = 'testuser'
   end
 
   def parse_request(request)
     uri = Addressable::URI.parse request.uri
-    e = @general_template.extract(uri)
+    e = @general_t.extract(uri)
     '/' + e['segments'].join('/')
   end
 
   def parse_rename_request(request)
     uri = Addressable::URI.parse request.uri
-    e = @rename_template.extract(uri)
+    e = @rename_t.extract(uri)
     source = '/' + e['segments'].join('/')
     dest = '/' + e['destination'].join('/')
     [source, dest]
   end
 
-  def stub_requests
-    @general_template = Addressable::Template.new "http://#{@hostname}:#{@port}/webhdfs/v1{/segments*}?op={op}&user.name={user}"
+  def stub_liststatus
+    liststatus_t = Addressable::Template.new "http://#{@host}:#{@port}" +
+      '/webhdfs/v1{/segments*}?op=LISTSTATUS&user.name={user}'
+    stub_request(:get, liststatus_t)
+      .to_return(body: lambda do |request|
+                         hdfs_path = parse_request(request)
+                         JSON.generate(@mockhdfs.ls(hdfs_path))
+                       end,
+                 headers: @ctheader)
+  end
 
-    liststatus_template = Addressable::Template.new "http://#{@hostname}:#{@port}/webhdfs/v1{/segments*}?op=LISTSTATUS&user.name={user}"
-    stub_request(:get, liststatus_template).to_return(body: lambda do |request|
-                                                              hdfs_path = parse_request(request)
-                                                              JSON.generate(@mockhdfs.ls(hdfs_path))
-                                                            end,
-                                                      headers: @ctheader)
+  def stub_getfilestatus
+    getfilestatus_t = Addressable::Template.new "http://#{@host}:#{@port}" +
+      '/webhdfs/v1{/segments*}?op=GETFILESTATUS&user.name={user}'
+    stub_request(:get, getfilestatus_t)
+      .to_return(body: lambda do |request|
+                         hdfs_path = parse_request request
+                         JSON.generate(@mockhdfs.stat(hdfs_path))
+                       end,
+                 headers: @ctheader)
+  end
 
-    getfilestatus_template = Addressable::Template.new "http://#{@hostname}:#{@port}/webhdfs/v1{/segments*}?op=GETFILESTATUS&user.name={user}"
-    stub_request(:get, getfilestatus_template).to_return(body: lambda do |request|
-                                                                 hdfs_path = parse_request request
-                                                                 JSON.generate(@mockhdfs.stat(hdfs_path))
-                                                               end,
-                                                         headers: @ctheader)
+  def stub_delete
+    delete_t = Addressable::Template.new "http://#{@host}:#{@port}" +
+      '/webhdfs/v1{/segments*}?op=DELETE&user.name={user}'
+    stub_request(:delete, delete_t)
+      .to_return(body: lambda do |request|
+                         hdfs_path = parse_request request
+                         JSON.generate(@mockhdfs.delete(hdfs_path))
+                       end,
+                 headers: @ctheader)
+  end
 
-    delete_template = Addressable::Template.new "http://#{@hostname}:#{@port}/webhdfs/v1{/segments*}?op=DELETE&user.name={user}"
-    stub_request(:delete, delete_template).to_return(body: lambda do |request|
-                                                       hdfs_path = parse_request request
-                                                       JSON.generate(@mockhdfs.delete(hdfs_path))
-                                                     end,
-                                                     headers: @ctheader)
-
-    @rename_template = Addressable::Template.new "http://#{@hostname}:#{@port}/webhdfs/v1{/segments*}?destination={/destination*}&op=RENAME&user.name={user}"
-    stub_request(:put, @rename_template).to_return(body: lambda do |request|
-                                                           hdfs_source_path, hdfs_dest_path  = parse_rename_request request
-                                                           JSON.generate(@mockhdfs.rename(hdfs_source_path, hdfs_dest_path))
-                                                         end,
-                                                   headers: @ctheader)
+  def stub_rename
+    @rename_t = Addressable::Template.new "http://#{@host}:#{@port}" +
+      '/webhdfs/v1{/segments*}?destination={/destination*}&op=RENAME' +
+      '&user.name={user}'
+    stub_request(:put, @rename_t)
+      .to_return(body: lambda do |request|
+                         source, dest = parse_rename_request request
+                         JSON.generate(@mockhdfs.rename(source, dest))
+                       end,
+                 headers: @ctheader)
   end
 
   def setup_environment
-    ENV['HDFS_HOST'] = @hostname
+    ENV['HDFS_HOST'] = @host
     ENV['HDFS_PORT'] = @port
     ENV['HDFS_USERNAME'] = @username
   end
